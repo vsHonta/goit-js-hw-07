a9a6dce90ebbe67199f691ba8ae4948380cabf90
Viktor H <66110545+vsHonta@users.noreply.github.com>
2022-11-09 02:10:45 +0200

    7th homework done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const images = galleryItems.reduce((acc, item) => {
+    const result =
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}" 
+                    alt="${item.description}"
+                    data-source="${item.original}"
+                />
+            </a>
+        </div>`;
+    return acc += result
+}, ``)
+
+gallery.insertAdjacentHTML('afterbegin', images)
+
+const onClick = evt => {
+    evt.preventDefault()
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+    // const instance = basicLightbox.create(`
+    //     <img src="${evt.target.dataset.source}" width="800" height="600">
+    // `)
+    // console.log(evt.target.dataset.source);
+    // return instance.show()
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: '250',
+        captionPosition: 'bottom',
+        captionsData: 'alt',
+    })
+
+gallery.addEventListener('click', onClick)
+
+
+
+// console.log(lightbox);
